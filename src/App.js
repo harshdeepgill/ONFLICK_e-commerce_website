2758c91c1593e85f43889abd4ac2fc4aa3073478
vk10082000 <vamshikrishna99089@gmail.com>
2023-09-27 22:28:51 +0530

    Day1 task done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import SignInAndSignUp from "./Pages/SignInAndSignUp";
+
 
 
 function App() {
   return (
     <div className="App">
       <h1>Hi from Decisive-Duck</h1>
+      <SignInAndSignUp/>
     </div>
   );
 }

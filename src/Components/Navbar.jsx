667671a18a0952392f318901a88d8d0e3f93b230
Shivam Maurya <vibhamaurya011@gmail.com>
2023-09-27 10:09:06 +0530

    Submited

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
-const Navbar = () => {
+import logo from '../assets/logo.png'
+function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav style={{display:'flex', justifyContent:'space-around', alignItems:'center'}}>
+      <div>
+        <h2>Shopcart</h2>
+      </div>
+      <div style={{display:'flex', gap:"25px", alignItems:'center'}}>
+        <a style={{textDecoration:'none', color:'black', fontWeight:"bold"}} href="">Category</a>
+        <input type="text" placeholder='Search...' />
+        <button>Search</button>
+      </div>
+      <div style={{display:'flex', alignItems:'center', gap:'25px'}}>
+        <a style={{textDecoration:'none', color:'black', fontWeight:"bold"}} href="">A</a>
+        <a style={{textDecoration:'none', color:'black', fontWeight:"bold"}} href="">Cart</a>
+      </div>
+    </nav>
   )
 }
 

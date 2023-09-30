764bbb6f47a568c41e5927f315d63a11ee7d7aef
vk10082000 <vamshikrishna99089@gmail.com>
2023-09-30 18:19:06 +0530

    login & singup done

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import { BrowserRouter } from "react-router-dom";
 import AllRoutes from "./Components/AllRoutes";
 import Navbar from "./Components/Navbar";
 import ImageSlider from "./Components/ImageSlider";
+import SignInAndSignUp from "./Components/SignInAndSignUp";
+
+
 
 function App() {
   return (
     <div className="App">
-      {/* <PaymentPage /> */}
-      <BrowserRouter>
-        <Navbar />
-        <AllRoutes />
-      </BrowserRouter>
+    
+     <SignInAndSignUp/>
+    
     </div>
   );
 }
--- src/Components/SignInAndSignUp.jsx
+++ src/Components/SignInAndSignUp.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { Tabs, TabList, TabPanels, Tab, TabPanel, Button, Input, FormLabel, FormControl, Box, InputGroup, InputRightElement, useDisclosure } from '@chakra-ui/react';
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Button, Input, FormLabel, FormControl, Box, InputGroup, InputRightElement, useDisclosure, useToast } from '@chakra-ui/react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 import { login, register } from '../Redux/AuthReducer/action';
-import { LOGIN_SUCSESS } from '../Redux/AuthReducer/actionType';
+import { LOGIN_FAILURE, LOGIN_SUCSESS } from '../Redux/AuthReducer/actionType';
 import {
   Modal,
   ModalOverlay,
@@ -13,173 +13,232 @@ import {
   ModalBody,
   ModalCloseButton,
 } from '@chakra-ui/react'
+import { useForm } from "react-hook-form";
 
-const intialRegisteredData = {id:"",password:"",name:""}
-const initialLogin ={id:"",password:""}
+const intialRegisteredData = { id: "", password: "", name: "" }
+const initialLogin = { id: "", password: "" }
+const SignInAndSignUp = () => {
 
-
-
-const SignInAndSignUp = ({onClose,isOpen}) => {
-
-  const [registerdData,setRegiteredData] = useState(intialRegisteredData);
-  const [loginData,setLogin] = useState(initialLogin)
+  const [registerdData, setRegiteredData] = useState(intialRegisteredData);
+  const [loginData, setLogin] = useState(initialLogin)
 
   const [show, setShow] = useState(false)
   const handleClick = () => setShow(!show)
-  
-  const dipatch = useDispatch();
-  
 
-  const handleRegisterChange=(e)=>{
-    const {name,value} = e.target;
+  const dispatch = useDispatch();
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    setRegiteredData({...registerdData, [name]:value})
+  const handleRegisterChange = (e) => {
+    const { name, value } = e.target;
+
+    setRegiteredData({ ...registerdData, [name]: value })
 
 
   }
-  const handleLoginChange=(e)=>{
-      
-    const {name,value} = e.target;
-  
-  setLogin({...loginData,[name]:value})
+  const handleLoginChange = (e) => {
+
+    const { name, value } = e.target;
+
+    setLogin({ ...loginData, [name]: value })
   }
 
   const loginSubmit = () => {
-  
-  dipatch(login(loginData))
-  .then((res)=>{
-    console.log(res.data)
-    if(res.data.password ===loginData.password){
-        // dispatch({type:LOGIN_SUCSESS})
-        //success alert
-        console.log("password matched")
-    }
-    else{
 
-        console.log("not password matched")
-    }
-  }).catch((error)=>{
-    console.log(error)
+    dispatch(login(loginData))
+      .then((res) => {
+        console.log(res.data)
+        if (res.data.password === loginData.password) {
+          dispatch({ type: LOGIN_SUCSESS })
+         
+          toast({
+            title: "Sign In Successfully!",
+            position: "top-center",
+            status: "success",
+            duration: 2000,
+            isClosable: true,
+          });
+          
+        }
+        else {
+          
+          toast({
+            title: "Password do not match",
+            position: "top-center",
+            status: "error",
+            duration: 2000,
+            isClosable: true,
+          })
+         // console.log("not password matched")
+        }
+      }).catch((error) => {
+        //console.log(error)
+        //user not
+        dispatch({type: LOGIN_FAILURE}) 
+        toast({
+          title: "Account Not Found",
+          description: "Create a new account",
+          position: "top-center",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+        })
 
 
-  })
- 
-  
+      })
+
+
   }
-  const registerSubmit=()=>{
-dipatch(register(registerdData))
+  const registerSubmit = () => {
+    dispatch(register(registerdData)).then((res) => {
+      toast({
+        title: "Account Created Successfully.",
+        position: "top-center",
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
+      document.getElementById("register").reset();
+    })
+      .catch(() => {
+        toast({
+          title: "Something went wrong!",
+          position: "top-center",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+        });
+      })
   }
- 
+
 
   return (
 
 
-    <>
-    
+    <DIV>
+      <Button onClick={onOpen}>Trigger modal</Button>
 
-    <Modal   onClose={onClose} isOpen={isOpen} size="sm" isCentered>
-    <ModalOverlay
-      bg='none'
-      backdropFilter='auto'
-      backdropInvert='20%'
-      backdropBlur='1px'
-    />
-     <DIV >
-      <ModalContent bg={"#f5f6f6"} >
-      <ModalBody>
+      <Modal onClose={onClose} isOpen={isOpen} size="sm" isCentered  >
+        <ModalOverlay
+          bg='none'
+          backdropFilter='auto'
+          backdropInvert='1%'
+          backdropBlur='2px'
+        />
+       
+          <ModalContent bg={"#f5f6f6"} >
+            <ModalBody>
 
+
+              <Tabs variant="soft-rounded" id='tab'  >
+
+                <TabList display={"flex"} justifyContent={"center"}>
+                  <Tab>Login</Tab>
+                  <Tab>Signup</Tab>
+                </TabList>
+                <TabPanels>
+                  <TabPanel>
+                    <FormControl action="" width="100%" margin="auto">
+                      < FormLabel>Enter Email</ FormLabel>
+                      <Input
+                        type="text"
+                        placeholder='Enter Email'
+                        name='id'
+                        value={loginData.id}
+                        onChange={handleLoginChange}
+                      />
+                      < FormLabel>Enter Password</ FormLabel>
+
+                      <InputGroup size='md'>
+                        <Input
+                          pr='4.5rem'
+                          type={show ? 'text' : 'password'}
+                          placeholder='Enter password'
+                          name='password'
+                          value={loginData.password}
+                          onChange={handleLoginChange}
+                        />
+                        <InputRightElement width='4.5rem'>
+                          <Button h='1.75rem' size='xs' className='hide' onClick={handleClick}>
+                            {show ? 'Hide' : 'Show'}
+                          </Button>
+                        </InputRightElement>
+                      </InputGroup>
+
+
+                      {/* <Input type="submit" /> */}
+                      <Box display={'flex'} justifyContent={'center'} mt={'20px'}>
+                        <ModalFooter>
+                          <Button colorScheme='linkedin' width="60px" mr={3} onClick={onClose}>
+                            Close
+                          </Button>
+
+
+                          <Button type='submit' colorScheme='linkedin' width="60px" onClick={loginSubmit}>Login</Button>
+                        </ModalFooter>
+                      </Box>
+                    </FormControl>
+                  </TabPanel>
+                  <TabPanel>
+                    <FormControl  id='register'>
+                      < FormLabel>Enter Name</ FormLabel>
+                      <Input type="text"
+                        placeholder='Name'
+                        name='name'
+                        value={registerdData.name}
+                        onChange={handleRegisterChange}
+                      />
+                      < FormLabel>Enter Email</ FormLabel>
+                      <Input type="text"
+                        placeholder='Enter Email'
+                        name='id'
+                        value={registerdData.id}
+                        onChange={handleRegisterChange}
+                      />
+                      < FormLabel>Create Password</ FormLabel>
+                      <InputGroup size='md'>
+                        <Input
+                          pr='4.5rem'
+                          type={show ? 'text' : 'password'}
+                          placeholder='Enter password'
+                          name='password'
+                          value={registerdData.password}
+                          onChange={handleRegisterChange}
+                        />
+                        <InputRightElement width='4.5rem'>
+                          <Button h='1.75rem' size='xs' className='hide' onClick={handleClick}>
+                            {show ? 'Hide' : 'Show'}
+                          </Button>
+                        </InputRightElement>
+                      </InputGroup>
+                      {/* <Input type="submit" /> */}
+                      <Box display={'flex'} justifyContent={'center'} mt={'20px'}
+                      // fontSize={["sm", "md", "lg", "xl"]}
+                      >
+                        <ModalFooter>
+                          <Button colorScheme='linkedin' width={"70px"} mr={3} onClick={onClose}>
+                            Close
+                          </Button>
+                          <Button colorScheme='linkedin' width={"70px"} onClick={registerSubmit} type='submit'>Sigin Up</Button>
+
+                        </ModalFooter>
+                      </Box>
+                    </FormControl>
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
+
+            </ModalBody>
+          </ModalContent>
      
-      <Tabs variant="soft-rounded" id='tab' >
-      <ModalCloseButton color={"red"}  />
-        <TabList margin={"auto"}>
-          <Tab>Login</Tab>
-          <Tab>Signup</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <FormControl action="" width="100%" margin="auto">
-              < FormLabel>Enter Email</ FormLabel>
-              <Input
-                type="text"
-                placeholder='Enter Email'
-                name='id'
-                value={loginData.id}
-                onChange={handleLoginChange}
-              />
-              < FormLabel>Enter Password</ FormLabel>
+      </Modal>
+    </DIV>
 
-              <InputGroup size='md'>
-                <Input
-                  pr='4.5rem'
-                  type={show ? 'text' : 'password'}
-                  placeholder='Enter password'
-                  name='password'
-                  value={loginData.password}
-                  onChange={handleLoginChange}
-                />
-                <InputRightElement width='4.5rem'>
-                  <Button h='1.75rem' size='xs' className='hide' onClick={handleClick}>
-                    {show ? 'Hide' : 'Show'}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-
-
-              {/* <Input type="submit" /> */}
-              <Box display={'flex'} justifyContent={'center'} mt={'20px'}>
-                <Button type='submit' colorScheme='linkedin' width="60px"  onClick={loginSubmit}>Login</Button>
-              </Box>
-            </FormControl>
-          </TabPanel>
-          <TabPanel>
-            <FormControl  >
-              < FormLabel>Enter Name</ FormLabel>
-              <Input type="text" 
-              placeholder='Name' 
-              name='name'
-              value={registerdData.name}
-              onChange={handleRegisterChange}
-              />
-              < FormLabel>Enter Email</ FormLabel>
-              <Input type="text" 
-              placeholder='Enter Email' 
-            name='id'
-            value={registerdData.id}
-              onChange={handleRegisterChange}
-              />
-              < FormLabel>Create Password</ FormLabel>
-              <InputGroup size='md'>
-                <Input
-                  pr='4.5rem'
-                  type={show ? 'text' : 'password'}
-                  placeholder='Enter password'
-                 name='password'
-                 value={registerdData.password}
-                  onChange={handleRegisterChange}
-                />
-                <InputRightElement width='4.5rem'>
-                  <Button h='1.75rem' size='xs' className='hide' onClick={handleClick}>
-                    {show ? 'Hide' : 'Show'}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-              {/* <Input type="submit" /> */}
-              <Box display={'flex'} justifyContent={'center'} mt={'20px'}
-              // fontSize={["sm", "md", "lg", "xl"]}
-              >
-                <Button colorScheme='linkedin' onClick={registerSubmit} type='submit'>Register</Button>
-              </Box>
-            </FormControl>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    
-    </ModalBody>
-      </ModalContent>
-      </DIV>
-    </Modal>
-  </>    
-    
   )
 }
 
@@ -187,50 +246,5 @@ export default SignInAndSignUp
 const DIV = styled.div`
 
 
-/* border: 1px solid red; */
- #tab{
- 
-  width: 100%;
-  
-  flex-direction: column;
-  align-items: center;
-  
-
-}
-
-/*
-input{
-  font-size: 12px;
-}
-label{
-  font-size: 13px;
-}
-button{
-  font-size: 16px;
-}
-.hide{
-  font-size: 10px;
-} */
-
-/* @media screen and (min-width: 769px) and (max-width: 1024px) {
-    #tab {
-      width: 100%;
-    }
-  }
-  @media screen and (min-width: 481px) and (max-width: 768px) {
-    #tab {
-      width: 60%;
-    }
-  }
-  @media screen and (min-width: 320px) and (max-width: 480px) {
-    #tab {
-      width: 90%;
-    }
-  }
-  @media screen and (min-width: 0px) and (max-width: 320px) {
-    #tab {
-      width: 100%;
-    }
-  } */
 
 `

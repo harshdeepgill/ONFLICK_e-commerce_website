cd7e76b96b9e031d0be7627115019c25b354c767
Harshdeep Gill <111063626+harshdeepgill@users.noreply.github.com>
2023-10-01 14:27:31 +0530

    Update SignInAndSignUp.jsx
    Vamishi Page correction

--- src/Components/SignInAndSignUp.jsx
+++ src/Components/SignInAndSignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Tabs, TabList, TabPanels, Tab, TabPanel, Button, Input, FormLabel, FormControl, Box, InputGroup, InputRightElement, useToast, ModalHeader, Heading, ModalCloseButton, InputLeftElement, Link, Text, Flex } from '@chakra-ui/react';
 import styled from 'styled-components';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { login, register } from '../Redux/AuthReducer/action';
 import { LOGIN_FAILURE, LOGIN_SUCSESS, SIGNUP_SUCCESS } from '../Redux/AuthReducer/actionType';
 import {
@@ -11,23 +11,15 @@ import {
   ModalFooter,
   ModalBody,
 } from '@chakra-ui/react'
-
-import { useNavigate } from 'react-router-dom';
-
 import { BsEnvelopeAt, BsEye, BsEyeSlash, BsPerson } from "react-icons/bs";
 import Logo from './Logo';
-
 
 const intialRegisteredData = { id: "", password: "", name: "",wishlist:[], cart:[], orders:[], image:""}
 const initialLogin = { id: "", password: "" }
 
 
 
-
-
-const SignInAndSignUp = ({ onClose, isOpen }) => {
-  const { isAuth, isloading, isError } = useSelector((store) => store.authReducer)
-
+const SignInAndSignUp = ({onClose,isOpen}) => {
 
   const [registerdData, setRegiteredData] = useState(intialRegisteredData);
   const [loginData, setLogin] = useState(initialLogin)
@@ -37,10 +29,7 @@ const SignInAndSignUp = ({ onClose, isOpen }) => {
   const handleClick = () => setShow(!show)
 
   const dispatch = useDispatch();
-
-  const toast = useToast()
-const nav = useNavigate()
-
+  const toast = useToast();
 
   const handleRegisterChange = (e) => {
     const { name, value } = e.target;
@@ -59,12 +48,8 @@ const nav = useNavigate()
       .then((res) => {
         console.log(res.data)
         if (res.data.password === loginData.password) {
-
-        //  dispatch({ type: LOGIN_SUCSESS })
-
           dispatch({ type: LOGIN_SUCSESS, payload:res.data });
           localStorage.setItem("flickUser", JSON.stringify({isAuthFlick:true, id:res.data.id}));
-
           toast({
             title: "Sign In Successfully!",
             position: "top-center",
@@ -72,11 +57,10 @@ const nav = useNavigate()
             duration: 2000,
             isClosable: true,
           });
-          setLogin(initialLogin)
-
+          
         }
         else {
-
+          
           toast({
             title: "Password do not match",
             position: "top-center",
@@ -84,14 +68,10 @@ const nav = useNavigate()
             duration: 2000,
             isClosable: true,
           })
-          // console.log("not password matched")
+         // console.log("not password matched")
         }
       }).catch((error) => {
-
-        //console.log(error)
-        //user not
-        dispatch({ type: LOGIN_FAILURE })
-
+        dispatch({type: LOGIN_FAILURE}); 
         toast({
           title: "Account Not Found",
           description: "Create a new account",
@@ -118,9 +98,7 @@ const nav = useNavigate()
         duration: 2000,
         isClosable: true,
       });
-
       setRegiteredData(intialRegisteredData);
-
     })
       .catch((err) => {
         console.log(err)
@@ -150,40 +128,6 @@ const nav = useNavigate()
           backdropInvert='1%'
           backdropBlur='2px'
         />
-
-
-//         <ModalContent bg={"#f5f6f6"} >
-//           <ModalBody>
-
-
-//             <Tabs variant="soft-rounded" id='tab'  >
-
-//               <TabList display={"flex"} justifyContent={"center"}>
-//                 <Tab>Login</Tab>
-//                 <Tab>Signup</Tab>
-//               </TabList>
-//               <TabPanels>
-//                 <TabPanel>
-//                   <FormControl action="" width="100%" margin="auto">
-//                     < FormLabel>Enter Email</ FormLabel>
-//                     <Input
-//                       type="text"
-//                       placeholder='Enter Email'
-//                       name='id'
-//                       value={loginData.id}
-//                       onChange={handleLoginChange}
-//                       required
-//                     />
-//                     < FormLabel>Enter Password</ FormLabel>
-
-//                     <InputGroup size='md'>
-//                       <Input
-//                         pr='4.5rem'
-//                         type={show ? 'text' : 'password'}
-//                         placeholder='Enter password'
-//                         name='password'
-//                         value={loginData.password}
-
        
           <ModalContent  minHeight={window.innerHeight*0.8} bg={"#f5f6f6"} >
             <ModalHeader>{tabIndex==0? <div style={{fontSize:"1.5rem"}}><Logo size={"2rem"}/></div>:<div><Logo size={"2rem"}/></div>}</ModalHeader>
@@ -211,59 +155,8 @@ const nav = useNavigate()
                         placeholder='example@mail.com'
                         name='id'
                         value={loginData.id}
-
                         onChange={handleLoginChange}
-                        required
                       />
-// <<<<<<< fp10_130_day_4
-//                       <InputRightElement width='4.5rem'>
-//                         <Button h='1.75rem' size='xs' className='hide' onClick={handleClick}>
-//                           {show ? 'Hide' : 'Show'}
-//                         </Button>
-//                       </InputRightElement>
-//                     </InputGroup>
-
-
-//                     {/* <Input type="submit" /> */}
-//                     <Box display={'flex'} justifyContent={'center'} mt={'20px'}>
-//                       <ModalFooter>
-//                         <Button colorScheme='linkedin' width="60px" mr={3} onClick={onClose}>
-//                           Close
-//                         </Button>
-
-
-//                         <Button type='submit' colorScheme='linkedin' width="60px" onClick={loginSubmit}>Login</Button>
-//                       </ModalFooter>
-//                     </Box>
-//                   </FormControl>
-//                 </TabPanel>
-//                 <TabPanel>
-//                   <FormControl id='register'>
-//                     < FormLabel>Enter Name</ FormLabel>
-//                     <Input type="text"
-//                       placeholder='Name'
-//                       name='name'
-//                       value={registerdData.name}
-//                       onChange={handleRegisterChange}
-//                       required
-//                     />
-//                     < FormLabel>Enter Email</ FormLabel>
-//                     <Input type="text"
-//                       placeholder='Enter Email'
-//                       name='id'
-//                       value={registerdData.id}
-//                       onChange={handleRegisterChange}
-//                       required
-//                     />
-//                     < FormLabel>Create Password</ FormLabel>
-//                     <InputGroup size='md'>
-//                       <Input
-//                         pr='4.5rem'
-//                         type={show ? 'text' : 'password'}
-//                         placeholder='Enter password'
-//                         name='password'
-//                         value={registerdData.password}
-
                       </InputGroup>
                       < FormLabel>Enter Password</ FormLabel>
 
@@ -325,38 +218,8 @@ const nav = useNavigate()
                         placeholder='example@mail.com'
                         name='id'
                         value={registerdData.id}
-
                         onChange={handleRegisterChange}
-                        required
                       />
-// <<<<<<< fp10_130_day_4
-//                       <InputRightElement width='4.5rem'>
-//                         <Button h='1.75rem' size='xs' className='hide' onClick={handleClick}>
-//                           {show ? 'Hide' : 'Show'}
-//                         </Button>
-//                       </InputRightElement>
-//                     </InputGroup>
-//                     {/* <Input type="submit" /> */}
-//                     <Box display={'flex'} justifyContent={'center'} mt={'20px'}
-//                     // fontSize={["sm", "md", "lg", "xl"]}
-//                     >
-//                      <ModalFooter>
-//                         <Button colorScheme='linkedin' width={"70px"} mr={3} onClick={onClose}>
-//                           Close
-//                         </Button>
-//                         <Button colorScheme='linkedin' width={"70px"} onClick={registerSubmit} type='submit'>Sigin Up</Button>
-
-//                       </ModalFooter> 
-//                     </Box>
-//                   </FormControl>
-//                 </TabPanel>
-//               </TabPanels>
-//             </Tabs>
-
-//           </ModalBody>
-//         </ModalContent>
-
-
                       </InputGroup>
                       < FormLabel>Create Password</ FormLabel>
                       <InputGroup size='md'>
@@ -386,7 +249,6 @@ const nav = useNavigate()
             </ModalFooter>
           </ModalContent>
      
-
       </Modal>
     </DIV>
 
@@ -395,44 +257,6 @@ const nav = useNavigate()
 
 export default SignInAndSignUp
 const DIV = styled.div`
-/* Loading css */
-svg {
- width: 3.25em;
- transform-origin: center;
- animation: rotate4 2s linear infinite;
-}
-
-circle {
- fill: none;
- stroke: hsl(214, 97%, 59%);
- stroke-width: 2;
- stroke-dasharray: 1, 200;
- stroke-dashoffset: 0;
- stroke-linecap: round;
- animation: dash4 1.5s ease-in-out infinite;
-}
-
-@keyframes rotate4 {
- 100% {
-  transform: rotate(360deg);
- }
-}
-
-@keyframes dash4 {
- 0% {
-  stroke-dasharray: 1, 200;
-  stroke-dashoffset: 0;
- }
-
- 50% {
-  stroke-dasharray: 90, 200;
-  stroke-dashoffset: -35px;
- }
-
- 100% {
-  stroke-dashoffset: -125px;
- }
-}
 
 
 

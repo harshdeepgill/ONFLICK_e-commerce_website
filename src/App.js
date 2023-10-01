6fac73457bd8cbed5431741800546549ec095920
jitu gandhare <jitugandhare@gmail.com>
2023-10-01 12:22:12 +0530

    footer component is done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import AllRoutes from "./Components/AllRoutes";
 import Navbar from "./Components/Navbar";
 import ImageSlider from "./Components/ImageSlider";
+import { Footer } from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <AllRoutes />
+     <Footer/>
     </div>
   );
 }

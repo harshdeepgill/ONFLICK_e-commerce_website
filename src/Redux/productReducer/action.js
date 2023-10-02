e260a2f10fbeebac2d613184ba03c068a0fbe6a9
harshdeepgill <er.harshdeepgill@gmail.com>
2023-10-02 14:04:42 +0530

    productList page complete

--- src/Redux/productReducer/action.js
+++ src/Redux/productReducer/action.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { GET_FAILEAR, GET_REQUEST, GET_SUCCESS } from "./actionType"
+import { END_OF_PRODUCT_LIST, GET_FAILEAR, GET_NEW_SUCCESS, GET_REQUEST, GET_SUCCESS } from "./actionType"
 
 export const getProducts = (parameter) => (dispatch) =>{
     dispatch({type:GET_REQUEST})
@@ -7,7 +7,23 @@ export const getProducts = (parameter) => (dispatch) =>{
         params: parameter
     })
     .then((res)=>{
-        dispatch({type:GET_SUCCESS, payload:res.data})
+        if(res.data.length == 0){
+            dispatch({type: END_OF_PRODUCT_LIST})
+        }else{
+            dispatch({type:GET_SUCCESS, payload:res.data})
+        }
+    })
+    .catch((err)=>{
+        dispatch({type:GET_FAILEAR})
+    })
+}
+
+export const getFirstProducts = (parameter) => (dispatch) => {
+    axios.get(`https://productdecisiveduck.onrender.com/products`, {
+        params: parameter
+    })
+    .then((res)=>{
+        dispatch({type:GET_NEW_SUCCESS, payload:res.data})
     })
     .catch((err)=>{
         dispatch({type:GET_FAILEAR})
--- src/Redux/productReducer/reducer.js
+++ src/Redux/productReducer/reducer.js
@@ -1,7 +1,8 @@
-import { GET_FAILEAR, GET_REQUEST, GET_SUCCESS } from "./actionType";
+import { END_OF_PRODUCT_LIST, GET_FAILEAR, GET_NEW_SUCCESS, GET_REQUEST, GET_SUCCESS } from "./actionType";
 const initialState = {
     isLoading:false,
     isError:false,
+    isFinished: false,
     products:[]
 }
 export const reducer = (state=initialState, {type, payload})=>{
@@ -9,9 +10,15 @@ export const reducer = (state=initialState, {type, payload})=>{
         case GET_REQUEST:
             return {...state, isLoading:true}
         case GET_SUCCESS:
-            return {...state, isLoading:false, products:[...state.products,...payload]}
+            return {...state, isLoading:false, isFinished:false, products:[...state.products,...payload]}
+        case GET_NEW_SUCCESS:
+            return {...state, isLoading:false,isFinished:false, products:payload}
         case GET_FAILEAR:
             return {...state, isLoading:false, isError:true}
+        case END_OF_PRODUCT_LIST: return {
+            ...state,
+            isFinished: true
+        }
         default:
             return state
     }

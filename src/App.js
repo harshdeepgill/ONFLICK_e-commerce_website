8f2dc34d0687ae9a3ebf34657e6684b9525a05a3
jitu gandhare <jitugandhare@gmail.com>
2023-10-02 14:45:02 +0530

    landingpage skeleton is done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AllRoutes from "./Components/AllRoutes";
 import Navbar from "./Components/Navbar";
 import ImageSlider from "./Components/ImageSlider";
 import { Footer } from "./Components/Footer/Footer";
-
+import LandingPageSkeleton from "./Components/LandingPageSkeleton";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
       <AllRoutes />
      <Footer/>
+      
     </div>
   );
 }
---
+++ src/Components/LandingPageSkeleton.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Skeleton, Box, Grid, GridItem } from "@chakra-ui/react";
+
+const LandingPageSkeleton = () => {
+  return (
+    <Grid
+      templateColumns="repeat(3, 1fr)"
+      gap={6}
+      p={4}
+      justifyItems="center"
+      alignItems="center"
+    >
+      {[...Array(6)].map((_, index) => (
+        <GridItem key={index}>
+          <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md">
+            <Skeleton height="200px" width="100%" mb={4} />
+            <Skeleton height="10px" width="200px" mb={2} />
+            <Skeleton height="10px" width="60%" />
+          </Box>
+        </GridItem>
+      ))}
+    </Grid>
+  );
+};
+
+export default LandingPageSkeleton;

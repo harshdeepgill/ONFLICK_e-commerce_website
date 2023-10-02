fc7219697ba9129a86bd31a4b43b25a34a5afe61
harshdeepgill <er.harshdeepgill@gmail.com>
2023-10-03 03:33:05 +0530

    deployment

--- src/Components/NavCatCard.jsx
+++ src/Components/NavCatCard.jsx
@@ -1,14 +1,17 @@
 import { Flex, Heading, Image } from '@chakra-ui/react'
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
 
 const NavCatCard = ({imageUrl, text}) => {
   return (
-    <Flex>
-        <Image w={"60px"} src={imageUrl}/>
-        <Flex w={"150px"} h={"60px"} justifyContent={"center"} alignItems={"center"}>
-            <Heading as='h5' size='sm'>{text}</Heading>
-        </Flex>
-    </Flex>
+    <Link to={`/productlist?category=${text}`}>
+      <Flex>
+          <Image w={"60px"} src={imageUrl}/>
+          <Flex w={"150px"} h={"60px"} justifyContent={"center"} alignItems={"center"}>
+              <Heading as='h5' size='sm'>{text}</Heading>
+          </Flex>
+      </Flex>
+    </Link>
   ) 
 }
 

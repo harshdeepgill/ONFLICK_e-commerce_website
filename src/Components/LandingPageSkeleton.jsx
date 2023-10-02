c25e5125d22893d7c62ea06f817e7c41be011252
jitu gandhare <jitugandhare@gmail.com>
2023-10-02 17:28:02 +0530

    LandingPageskeleton is done

--- src/Components/LandingPageSkeleton.jsx
+++ src/Components/LandingPageSkeleton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Skeleton, Box, Grid, GridItem } from "@chakra-ui/react";
+import { auto } from "@popperjs/core";
 
 const LandingPageSkeleton = () => {
   return (
@@ -21,16 +22,29 @@ const LandingPageSkeleton = () => {
         <Skeleton height="30px" width="40px" />
         <Skeleton height="30px" width="40px" />
       </Box>
-
       <Box
         bg="gray.200"
         color="white"
         p={4}
         paddingTop={20}
+        pb={10}
         display="flex"
         justifyContent="space-between"
         alignItems="center"
-        height={500}
+        height={200}
+        margin={auto}
+        width="100%"
+      ></Box>
+      <Box
+        bg="gray.200"
+        color="white"
+        marginTop={50}
+        p={4}
+        paddingTop={20}
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        height={400}
         width="100%"
       ></Box>
     </div>
